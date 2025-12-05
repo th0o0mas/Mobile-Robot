5e2a66dbb971d296ae86b42d692acb4231c89560
th0o0mas <104323318+th0o0mas@users.noreply.github.com>
2025-12-05 21:57:47 +0800

    Small Excel issue

--- datas/records/process_data.py
+++ datas/records/process_data.py
@@ -1,8 +1,8 @@
 import re
 
 # Input and output file paths
-input_file = r"C:\Users\thomm\.vscode\Folders\Github\Mobile-Robot\datas\records\Tom_meilleur.txt" #To replace with actual path
-output_file = r"C:\Users\thomm\.vscode\Folders\Github\Mobile-Robot\datas\records\best.csv" #To replace with actual path
+input_file = r"C:\Users\thomm\.vscode\Folders\Github\Mobile-Robot\datas\records\File_to_edit_to_turn_into_csv.txt" #To replace with actual path
+output_file = r"C:\Users\thomm\.vscode\Folders\Github\Mobile-Robot\datas\records\File_to_edit_to_turn_into_csv_processed.csv" #To replace with actual path
 
 # Read the input file
 with open(input_file, 'r', encoding='utf-8-sig') as f:
