4b240b7abcc3773afeb84485e3e509587a40241a
th0o0mas <104323318+th0o0mas@users.noreply.github.com>
2025-12-05 12:42:21 +0800

    Ask prompt for gain

--- MecanumWMR/main.cpp
+++ MecanumWMR/main.cpp
@@ -17,6 +17,7 @@ int main(int argc, char** argv) {
     ControllerParameter param = wmr.getControllerParam();
     std::array<float, 3> target_point = {0.0};
     for (int i=0;i<3;i++) {
+        std::cout << "kp=" << param.kp[0] << ", ki=" << param.ki[0] << ", kd=" << param.kd[0] << ", kb=" << param.kb[0] << std::endl;
         std::cout<<"Please enter a target point [x, y, theta]: ";
         std::cin>>target_point[0]>>target_point[1]>>target_point[2];
         wmr.Point2PointMove(target_point);
@@ -27,9 +28,19 @@ int main(int argc, char** argv) {
         std::cin >> choice;
         if (choice == 'y' || choice == 'Y') {
             float kp, ki, kd, kb;
-            std::cout << "Enter new PID gains for translation (kp[0] ki[0] kd[0] kb[0]): ";
-            std::cin >> kp >> ki >> kd >> kb;
-            wmr.setPIDgain_translation(kp, ki, kd, kb);
+            std::cout << "Which gain would you like to change (0 = translation, 1 = angular rotation )? "<< std::endl;
+            int axis;
+            std::cin >> axis;
+            std::cout << "Enter new gains (kp ki kd kb): ";
+            if (axis == 0) {
+                std::cin >> kp >> ki >> kd >> kb;
+                wmr.setPIDgain_translation(kp, ki, kd, kb);
+            }
+            else
+            {
+                std::cin >> kp >> ki >> kd >> kb;
+                wmr.setPIDgain_rotation(kp, ki, kd, kb);
+            }
         }
         
     
